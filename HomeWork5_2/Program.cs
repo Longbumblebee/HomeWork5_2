3f38c039ba309f23aa576f5114c67386838841dc
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-10 22:27:48 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTimeText = $"{DateTime.Now.ToString()} - время позорного копирования";
+            File.WriteAllText("startup.txt", startTimeText);
+       
+           
+
+        }
+    }
+}
